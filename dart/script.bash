eb86af7709fe8c426a7ca259dc19c2c89d343eb4
nungpalm <77908221+nungpalm@users.noreply.github.com>
2026-04-24 17:42:44 +0700

    work 2

--- dart/script.bash
+++ dart/script.bash
@@ -9,9 +9,4 @@ dart test
 
 docker build -t dartja:${TAG} .
 
-docker build -t dartja:latest .
-
-
 docker run -d -p 8086:8086 --name dartja -e DATABASE_URI=${DATABASE_URI} -e REDIS_ENDPOINT=${REDIS_ENDPOINT} dartja:${TAG}
-
-docker run -d -p 8086:8086 --name dartja -e DATABASE_URI=11.1.111.11 -e REDIS_ENDPOINT=22.2.222.22 dartja:latest
